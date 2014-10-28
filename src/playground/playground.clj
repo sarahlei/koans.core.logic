1fb3f244827571e5c73e82c121dc646beb118752
Gergely Nagy <algernon@madhouse-project.org>
2014-10-28 15:47:26 +0100

    playground: Pull in more goodies
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/playground/playground.clj
+++ src/playground/playground.clj
@@ -1,6 +1,7 @@
 (ns playground
   (:refer-clojure :exclude [==])
   (:require [clojure.core.logic :refer :all]
+            [clojure.core.logic.fd :as fd]
+            [clojure.core.logic.pldb :as pldb]
             [playground.extra :refer :all]
             [clojure.tools.macro :as macro]))
-
