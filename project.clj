2924aa3aac21882372ac2a520c07a6c3aac21b0e
Gergely Nagy <algernon@madhouse-project.org>
2014-10-28 14:26:39 +0100

    project.clj: Add tools.macro
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
             :url "http://www.gnu.org/licenses/lgpl.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [koan-engine "0.2.0"]
-                 [org.clojure/core.logic "0.8.8"]]
+                 [org.clojure/core.logic "0.8.8"]
+                 [org.clojure/tools.macro "0.1.2"]]
   :dev-dependencies [[lein-koan "0.1.2"]]
   :plugins [[lein-koan "0.1.2"]])
