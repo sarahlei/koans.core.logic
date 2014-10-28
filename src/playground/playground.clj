494689e63628ff312967b29e99605ceee939d05a
Gergely Nagy <algernon@madhouse-project.org>
2014-10-28 14:54:26 +0100

    playground: Add an empty playground
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ src/playground/playground.clj
@@ -0,0 +1,6 @@
+(ns playground
+  (:refer-clojure :exclude [==])
+  (:require [clojure.core.logic :refer :all]
+            [playground.extra :refer :all]
+            [clojure.tools.macro :as macro]))
+
